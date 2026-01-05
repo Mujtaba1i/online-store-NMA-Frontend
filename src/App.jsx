29270afb8e767e779f09e978b9883a46641ae291
Mujtaba1i <unmujt@gmail.com>
2026-01-05 10:08:31 +0300

    template

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { Route,Routes } from 'react-router'
-import HomePage from './components/HomePage/HomePage'
-import Navbar from './components/Navbar/Navbar'
-import styles from './App.module.css'
+import NavBar from "./components/NavBar/NavBar"
+import SignUpForm from "./components/SignUpForm/SignUpForm"
+import Homepage from "./components/Homepage/Homepage"
+import { Route,Routes } from "react-router"
+import { UserContext } from "./contexts/UserContext"
+import { useContext } from 'react'
+import SignInForm from "./components/SignInForm/SignInForm"
 
 function App() {
+
+  const {user} = useContext(UserContext)
+
   return (
-      <>
-      <Navbar/>
-      <div className='testdivdiv'>NMA</div>
+    <>
+    <div>
+      <NavBar/> 
+      <h1>MAMAMIA!! {user ? ', '+user.username : ''}</h1> 
       <Routes>
-        <Route path='/' element=<HomePage/> />
+        <Route path="/" element={<Homepage/>}/>
+        <Route path="/Sign-up" element={<SignUpForm/>}/>
+        <Route path="/Sign-in" element={<SignInForm/>}/>
       </Routes>
-      </>
+    </div>
+    </>
   )
 }
 
---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,65 @@
+import { useContext, useState } from "react"
+import { signUp } from "../../services/authService";
+import { useNavigate } from "react-router";
+import { UserContext } from "../../contexts/UserContext";
+
+function SignUpForm() {
+    const navigate = useNavigate()
+    const {setUser} = useContext(UserContext)
+
+    const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+    const { username, password, confirmPassword } = formData;
+    
+    function handleChange(event){
+        setFormData({...formData,[event.target.name]:event.target.value})
+        
+    }
+    
+    async function handleSubmit (event){
+        event.preventDefault()
+        delete formData.confirmPassword
+        const response = await signUp(formData)
+        if (response){
+            setUser(response)
+            navigate('/')
+        }
+        else{
+            console.log('Ran into an error');
+            setFormData({
+                username: '',
+                password: '',
+                confirmPassword: '',
+            })
+        }
+        
+    }
+
+    const isFormInvalid = () => !(username && password && password === confirmPassword)
+
+  return (
+    <div>
+        <h1>Signup</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username: </label>
+            <input name="username" id="username" type="text" onChange={handleChange} value={username}/><br /><br />
+            
+            <label htmlFor="password">password: </label>
+            <input name="password" id="password" type="password" onChange={handleChange} value={password}/><br /><br />
+
+            <label htmlFor="confirm-password">confirm password: </label>
+            <input name="confirmPassword" id="confirm-password" type="password" onChange={handleChange} value={confirmPassword}/><br /><br />
+            
+            <button disabled={isFormInvalid()}>Sign Up</button><br /><br />
+            <button onClick={() => navigate('/')}>Cancel</button>
+
+        </form>
+    </div>
+  )
+}
+
+export default SignUpForm
